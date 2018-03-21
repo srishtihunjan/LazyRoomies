b5002e963bc5f6d8bf2a09b034cd4c16dfb57d69
gdeepchadha <gdeepchadha@ufl.edu>
2018-03-21 01:18:03 -0400

    temporary changes

--- React/src/components/ListItems/ListItem/ListItem.js
+++ React/src/components/ListItems/ListItem/ListItem.js
@@ -2,10 +2,18 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 
 const listItem = (props) => {
-    
+    if(props.item === ''){
         return (
             <TextField id={props.index.toString()}
-            defaultValue={props.item} />
+            onChange={props.inputChangeHandler}
+            onKeyPress={props.enterPressedHandler}/>
+        ); 
+    }
+        return (
+            <TextField id={props.index.toString()}
+            defaultValue={props.item === ''? (" yo"+props.index.toString()) : props.item} 
+            onChange={props.inputChangeHandler}
+            onKeyPress={props.enterPressedHandler}/>
         );        
 }
 
--- React/src/components/ListItems/ListItems.js
+++ React/src/components/ListItems/ListItems.js
@@ -4,7 +4,11 @@ import ListItem from './ListItem/ListItem';
 const listItems = (props) => {
 
     let items = props.listItems.map((item, index) => {
-        return <ListItem key={index} index={index} item={item}/>;
+        return <ListItem key={index} 
+        index={index} 
+        item={item} 
+        inputHandler={(event) => props.inputChangeHandler(event, index)}
+        enterPressedHandler={(env) => props.enterPressedHandler(env, index)} />;
     });
     console.log(items);
     return (items);
