2706d18c0aeb952c0db106402b6c7539d5952b43
yashojhamvar <yasho170@gmail.com>
2018-04-01 18:17:35 -0400

    App.js resovled conflict

--- React/src/App.js
+++ React/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
-<<<<<<< HEAD
-import TaskManager from './containers/TaskManager/TaskManager';
-import ShoppingList from './containers/ShoppingList/ShoppingList';
-=======
 import Root from './containers/Root/Root';
 import { BrowserRouter } from 'react-router-dom';
->>>>>>> 9c695d09cdcd939d05f236c1e514252151c9c892
+
 
 class App extends Component {
   render() {
