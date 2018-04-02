51fe646199b2087fbf121040298327307a43fbdf
Srishti Hunjan <srishti.hunjan@gmail.com>
2018-04-01 23:08:37 -0400

    Route Updation:  - Login user with email and password validation  - Signup user, apt optional  - Join apartment  - Get all users in apartment  - Get apt info by id  - Get apt info by name

--- routes/apartments.js
+++ routes/apartments.js
@@ -11,23 +11,42 @@ var dbQuery = require('../db_setup/db_setup');
 var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
-/* GET users listing. */
-router.get('/:apartmentId', function (req, res, next) {
+/* GET apartment details by Id */
+router.get('/id/:id', function (req, res, next) {
   dbQuery(function (db) {
-    apartment.findById(req.params.apartmentId, function (err, response) {
+    apartment.findById(req.params.id, function (err, response) {
       if (err) {
         console.log(err);
-        res.statusCode(500).send("Error fetching apartment details");
+        res.status(500).send("Error fetching apartment details");
         db.close();
         return;
       }
       console.log('**Apartment Info queried!');
-      res.statusCode(200);
+      res.status(200);
       res.send(response);
       db.close();
     });
   });
 });
+
+/* GET apartment details by name */
+router.get('/name/:name', function (req, res, next) {
+  dbQuery(function (db) {
+    apartment.findById(req.params.apartmentId, function (err, response) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error fetching apartment details");
+        db.close();
+        return;
+      }
+      console.log('**Apartment Info queried!');
+      res.status(200);
+      res.send(response);
+      db.close();
+    });
+  });
+});
+
 
 router.post('/', function (req, res, next) {
   dbQuery((db) => {
@@ -36,28 +55,20 @@ router.post('/', function (req, res, next) {
     }, function (err1, response) {
       if (err1) {
         console.log(err1);
-        res.statusCode(500).send("Error creating apartment");
+        res.status(500).send("Error creating apartment");
         db.close();
         return;
       }
-      console.log('Apt created with ID = ' + response._id);
-      user.findByIdAndUpdate(req.body.userId, { apartmentID: response._id }, function (err2, update_response) {
-        if (err2) {
-          console.log('User ID not found or updated with apartmentID');
-          res.statusCode(500).send("Error updating apartment information in user");
-          db.close();
-          return;
-        }
-        if (update_response) {
-          console.log('User ID found and updated with apt info!');
-          res.sendStatus(201);
-        } else {
-          console.log('User NOT updated with apt info!!');
-          res.statusCode(400);
-          res.send('User NOT updated with apt info!!');
-        }
+      if (response != null && response != '') {
+        console.log('Apartment created! ' + response);
+        res.sendStatus(201);
         db.close();
-      });
+      } else {
+        console.log('Apartment NOT created! '+response);
+        res.status(400);
+        res.send('Apartment NOT created! Name is UNIQUE!');
+      }
+      db.close();
     });
   });
 });
