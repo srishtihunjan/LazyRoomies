35730c3e12a326e296efbec31ceb46e6fb4abf66
gdeepchadha <gdeepchadha@ufl.edu>
2018-05-24 19:49:36 -0400

    made changes to be able to deploy the code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LazyRoomies' });
+  res.sendFile(path.join(__dirname, '../React/build/index.html'));
 });
 
 module.exports = router;
