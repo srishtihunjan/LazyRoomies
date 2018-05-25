6e2c939a6e323aada0ca3fa001a96848999b25f6
gdeepchadha <gdeepchadha@ufl.edu>
2018-05-25 12:24:46 -0400

    made some changes for correct deployment

--- React/src/Config/Config.js
+++ React/src/Config/Config.js
@@ -1,1 +1,9 @@
-module.exports= {url: "http://localhost:8000/"};
+let url;
+switch(process.env.NODE_ENV){
+    case 'production':
+        url = "https://first-trial-gsc.herokuapp.com:80/";
+        break;
+    default:
+        url = "http://localhost:8000/";
+}
+module.exports = {url: url};
